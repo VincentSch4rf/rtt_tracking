43eb4428807d5e24e1535b53aa903f3e528bd886
brsu-youbot <robotics@h-brs.de>
2022-04-08 16:29:27 +0200

    Fix: Empty detections nms error

--- nanonets_object_tracking/not_main.py
+++ nanonets_object_tracking/not_main.py
@@ -114,10 +114,13 @@ def callback(data):
         now = time.time()
         bboxes, probs, labels = model.classify(image)
         detection_time = time.time() - now
-        # print('Detection', detection_time)
+        print('Detection', detection_time)
         # detections = convert_bboxes(bboxes)
         # Combines scores and bboxes, and converts to tlbr format
-        detections, labels = nms_adapted(bboxes, probs, labels)
+        if len(bboxes) > 0:     # We only do NMS if we have detections
+            detections, labels = nms_adapted(bboxes, probs, labels)
+        else:
+            detections, labels = np.empty((0, 5)), None
 
         if detections is None or len(detections.shape) == 0:
             print("No dets")
@@ -125,14 +128,14 @@ def callback(data):
         
         now = time.time()
         trackers, obj_classes = sort_tracker.update(detections, labels)  # This returns bbox and track_id
-        print("with dets, No. of tracks:", len(trackers))
+        #print("with dets, No. of tracks:", len(trackers))
     else:
         now = time.time()
         trackers, obj_classes = sort_tracker.update()
         
-        print("No det, No. of tracks:", len(trackers))
+        #print("No det, No. of tracks:", len(trackers))
         detection_time = time.time() - now
-        # print('Tracking', detection_time)
+        print('Tracking', detection_time)
 
     publish_annotations(image, trackers, obj_classes)
     j += 1
