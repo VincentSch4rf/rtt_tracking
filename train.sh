199c95f60ea82c9b6bad0b8d348c7436244251e5
Vincent Scharf <vincent.scharf@smail.inf.h-brs.de>
2022-04-16 18:31:45 +0200

    use rtt dataset

--- train.sh
+++ train.sh
@@ -4,21 +4,21 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR
+#SBATCH --nodelist=wr25
 
 module load cuda
 
-PROJECT=atwork_128_1280_low
+PROJECT=rtt_128_1280_high
 cd /home/vschar2s/bitbots/rtt_tracking || exit
 
 torchrun --standalone --nnodes=1 --nproc_per_node 4 ./yolov5/train.py \
 --project out/train \
 --name $PROJECT \
---workers 2 \
 --img 1280 \
---batch 128 \
+--batch 376 \
 --epochs 300 \
---hyp yolov5/data/hyps/hyp.scratch-low.yaml \
---data atwork.yaml \
+--hyp yolov5/data/hyps/hyp.scratch-high.yaml \
+--data rtt.yaml \
 --weights yolov5s.pt
 
 python ./yolov5/val.py \
@@ -26,6 +26,6 @@ python ./yolov5/val.py \
 --name $PROJECT \
 --task test \
 --img 1280 \
---data atwork.yaml \
+--data rtt.yaml \
 --weights out/train/$PROJECT/weights/best.pt
 
