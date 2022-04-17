cd67682f5150a8830d688c927cdf0d57c2dcb698
Vincent Scharf <vincent.scharf@smail.inf.h-brs.de>
2022-04-17 12:26:34 +0200

    update slurm launch scripts

--- evolve.sh
+++ evolve.sh
@@ -4,15 +4,23 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR
+#SBATCH --ntasks-per-node=60
 
-# module load cuda
+ module load cuda
 
 cd /home/vschar2s/bitbots/rtt_tracking || exit
 
+source /home/${USER}/.bashrc
+conda activate yolo
+
+NAME=atwork_1280_50_300
+
 python ./yolov5/train.py \
---workers 2 \
---img 640 \
---batch 32 \
+--workers 8 \
+--project out/evolve \
+--name NAME \
+--img 1280 \
+--batch -1 \
 --epochs 50 \
 --data atwork.yaml \
 --weights yolov5s.pt \
--- train.sh
+++ train.sh
@@ -1,31 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --partition=gpu4test
 #SBATCH --mem=460G
 #SBATCH --time=1-00:00:00
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR
-#SBATCH --nodelist=wr25
+#SBATCH --ntasks-per-node=120
 
 module load cuda
 
-PROJECT=rtt_128_1280_high
+NAME=rtt_376_1280_low_1500
 cd /home/vschar2s/bitbots/rtt_tracking || exit
 
+source /home/${USER}/.bashrc
+conda activate yolo
+
+export YOLOv5_VERBOSE=False
+
 torchrun --standalone --nnodes=1 --nproc_per_node 4 ./yolov5/train.py \
+--workers 8 \
 --project out/train \
---name $PROJECT \
+--name $NAME \
 --img 1280 \
 --batch 376 \
---epochs 300 \
---hyp yolov5/data/hyps/hyp.scratch-high.yaml \
+--epochs 1500 \
+--hyp yolov5/data/hyps/hyp.scratch-low.yaml \
 --data rtt.yaml \
---weights yolov5s.pt
+--weights yolov5s.pt \
+--cache
 
 python ./yolov5/val.py \
 --project out/val \
---name $PROJECT \
+--name $NAME \
 --task test \
 --img 1280 \
 --data rtt.yaml \
---weights out/train/$PROJECT/weights/best.pt
+--weights out/train/$NAME/weights/best.pt
+
+python ./yolov5/detect.py \
+--project out/val \
+--name $NAME \
+--img 1280 \
+--data rtt.yaml \
+--weights out/train/$NAME/weights/best.pt \
+--source /scratch/vschar2s/rtt/test/images \
+--agnostic-nms
 
